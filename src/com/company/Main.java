a22c0ece635cd25958fdad7021dfca0aff8311f7
Swierkowski.K@gmail.com <Krzysiu3285>
2021-11-21 17:27:28 +0100

    Initial Comit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class Main {
+    private static boolean shouldLoop = true;
 
     public static void main(String[] args) {
         Menu menu = new Menu();
@@ -8,12 +9,14 @@ public class Main {
         User user = new User();
         bookingPlace.mapParkingDefault();
 
-        while (true) {
+
+        while (shouldLoop) {
             switch (menu.menu()) {
                 case 1 -> user.adduser();
                 case 2 -> bookingPlace.booking();
                 case 3 -> bookingPlace.unbook();
                 case 4 -> bookingPlace.cancelOldbooking();
+                case 5 -> shouldLoop = false;
                 default -> {
                 }
             }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -11,9 +11,10 @@ public class Menu {
         System.out.println("Wybierz Opcję");
         System.out.println("*****************************\n");
         System.out.println("1 - Dodaj użytkownika");
-        System.out.println("2 - rezerwuj miejsce");
-        System.out.println("3 - anuluj rezerwacje");
-        System.out.println("4 - Czyszczenie rezerwacji na dzień następny\n");
+        System.out.println("2 - Rezerwuj miejsce");
+        System.out.println("3 - Anuluj rezerwacje");
+        System.out.println("4 - Czyszczenie rezerwacji na dzień następny");
+        System.out.println("5 - Zakończ\n");
         System.out.println("*****************************\n");
         option = input.nextInt();
         return option;
